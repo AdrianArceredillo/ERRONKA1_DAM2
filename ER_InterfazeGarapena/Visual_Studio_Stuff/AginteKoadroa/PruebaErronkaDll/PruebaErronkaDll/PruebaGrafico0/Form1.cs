cb90f81aa74a0ccfcd2f07faa1151d0eeabb1293
Adrian Arceredillo <arceredillo.adrian@uni.eus>
2022-10-25 13:29:49 +0200

    .

---
+++ ER_InterfazeGarapena/Visual_Studio_Stuff/AginteKoadroa/PruebaErronkaDll/PruebaErronkaDll/PruebaGrafico0/Form1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.SqlServer.Server;
+using PruebaGrafico0.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace PruebaGrafico0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (var db = new Ariketa_1DbContext())
+            {
+                var erosketenData = db.Producto
+                    .Include("Compra")
+                    .GroupBy(b => b.Id)
+                    .ToDictionary(g => g.Key, g => g.Count());
+
+                if (erosketenData != null)
+                {
+                    if (erosketenData.Count > 0)
+                    {
+                        var kontrolak = pruebaErronkaDll1.Controls.OfType<System.Windows.Forms.DataVisualization.Charting.Chart>(); foreach (var kontrola in kontrolak)
+                        {
+                            kontrola.Titles[0].Text = "Productos Comprados - Odoo";
+                            kontrola.DataSource = erosketenData;
+                            kontrola.Series[0].YValueMembers = "Value";
+                            kontrola.Series[0].XValueMember = "Key";
+                            kontrola.DataBind();
+                        }
+
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ ER_InterfazeGarapena/Visual_Studio_Stuff/AginteKoadroa/PruebaErronkaDll/PruebaErronkaDll/PruebaGrafico0/Models/Ariketa_1DbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace PruebaGrafico0.Models
+{
+    public class Ariketa_1DbContext : DbContext
+    {
+        public Ariketa_1DbContext() : base(nameOrConnectionString: "Ariketa_1DbContext")
+        { }
+        public DbSet<Producto> Producto { get; set; }
+        public DbSet<Compra> Compra { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("public");
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
