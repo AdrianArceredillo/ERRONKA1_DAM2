c568e85d4157bda96ee68ce9858efb3940b672da
Alain Basterra <basterra.alain@uni.eus>
2022-11-02 13:52:45 +0100

    Insert ondo eginda

--- Erronka1_Alain/Java/erronka1/src/main/java/com/exekutagarriak/idatzi.java
+++ Erronka1_Alain/Java/erronka1/src/main/java/com/exekutagarriak/idatzi.java
@@ -21,7 +21,51 @@ public class idatzi
     }
     public static void insertTaula(String taula){
         String sql = "INSERT INTO public.\"" + taula +"\" VALUES('Proba','12',NULL, true);"; // AAAAAAAAA taulan sartzeko datuak
-        //String sql = "INSERT INTO public.\"" + taula +"\" VALUES(NULL, NULL, 'Proba_Izena', 1, '<p><br></p>', NULL, NULL, 'product', 'product', 1, 420, 10, 10, true, 1, 1, NULL, true, NULL, NULL, false, false, 0, 2, '2022-10-26 08:32:55.509688', 0, 'none', NULL, NULL, NULL, 'receive', 'no-message', NULL, 'manual', 'no-message', NULL, 'no', 'order', NULL);";
+        /*INSERT INTO public."product_template"
+VALUES(
+	8,
+	NULL,
+	'Proba_Izena231478',
+	1,
+	'<p><br></p>',
+	NULL,
+	NULL,
+	'product',
+	'product',
+	1,
+	420,
+	10,
+	10,
+	true,
+	true,
+	1,
+	1,
+	NULL,
+	true,
+	NULL,
+	NULL,
+	false,
+	false,
+	0,
+	2,
+	'2022-10-26 08:32:55.509688',
+	2,
+	'2022-10-26 08:32:55.509688',
+	0,
+	'hgfg',
+	NULL,
+	NULL,
+	NULL,
+	'receive',
+	'no-message',
+	NULL,
+	'manual',
+	'no-message',
+	NULL,
+	'no',
+	'order',
+	false
+);*/
         //String sql = "INSERT INTO public.\"" + taula +"\" VALUES();"; 
         Konexioa konekzioa = new Konexioa();
         Statement st;
