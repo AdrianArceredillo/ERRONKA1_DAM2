3a531b65681996661e9f6da89e754b8e950a8fba
Haritz Otero <otero.haritz@uni.eus>
2022-11-16 10:56:00 +0100

    BOTOI Aldaketa

--- Grafikoak_Joseba/Form1.Designer.cs
+++ Grafikoak_Joseba/Form1.Designer.cs
@@ -110,11 +110,12 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(1369, 789);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(203, 87);
             this.button1.TabIndex = 9;
-            this.button1.Text = "SEE STOCK INFO";
+            this.button1.Text = "IKUSI STOCKAREN INFORMAZIOA";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
