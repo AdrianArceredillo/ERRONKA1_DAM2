da05144f04f684e5ec7cf7d9c4df72c458d722c3
Alain Basterra <basterra.alain@uni.eus>
2022-11-17 09:14:57 +0100

    miszelanea ezabatu

--- Erronka1_Alain/Java/erronka1/src/main/java/com/postgress/insert/ProduktuListaInportatu.java
+++ Erronka1_Alain/Java/erronka1/src/main/java/com/postgress/insert/ProduktuListaInportatu.java
@@ -22,14 +22,17 @@ public class ProduktuListaInportatu {
         System.out.print("Fitxategiaren izena (.csv, .xml edo .json formatukoa): ");
         String fitxategia = in.nextLine();
         produktuak = produktuenListaObjetura(fitxategia);
-        int kontagailua = 0;
-        String izena, deskripzioa, barraKodea, lehentasuna, idAk;
-        Float prezioa, bolumena, pisua, stocka;
-        int idPT = idLortuPT() + 1;
-        int idPP = idLortuPP() + 1;
-        int idSQ = idLortuSQ() + 1;
+        if (produktuak != null) {
 
-        for (Produktua p : produktuak.getProduktuak()) {
+            int kontagailua = 0;
+            String izena, deskripzioa, barraKodea, lehentasuna, idAk;
+            Float prezioa, bolumena, pisua, stocka;
+            int idPT = idLortuPT() + 1;
+            int idPP = idLortuPP() + 1;
+            int idSQ = idLortuSQ() + 1;
+
+            for (Produktua p : produktuak.getProduktuak()) {
+                System.out.println("Produktua(k) inportatzen...");
                 izena = p.getIzena();
                 deskripzioa = p.getDeskripzioa();
                 barraKodea = p.getBarraKodea();
@@ -42,12 +45,12 @@ public class ProduktuListaInportatu {
                         bolumena, pisua, stocka, lehentasuna);
                 kontagailua++;
                 String[] idArray = idAk.split(";");
-                idPT = Integer.parseInt(idArray[0])  + 1;
-                idPP = Integer.parseInt(idArray[1])  + 1;
-                idSQ = Integer.parseInt(idArray[2])  + 2;
+                idPT = Integer.parseInt(idArray[0]) + 1;
+                idPP = Integer.parseInt(idArray[1]) + 1;
+                idSQ = Integer.parseInt(idArray[2]) + 2;
+            }
+            System.out.println((kontagailua) + " produktu gehitu dira");
         }
-        System.out.println((kontagailua + 1) + " produktu gehitu dira");
-
     }
 
     public static Produktuak produktuenListaObjetura(String FileIn) {
@@ -80,9 +83,9 @@ public class ProduktuListaInportatu {
                 id = rs.getInt(1);
             }
         } catch (Exception ex) {
-            System.out.println("Exception (PT id): " + ex);
+            //System.out.println("Exception (PT id): " + ex);
         }
-        System.out.println("Lortutako PT id-a: " + id);
+        //System.out.println("Lortutako PT id-a: " + id);
         return id;
     }
 
@@ -96,9 +99,9 @@ public class ProduktuListaInportatu {
                 id = rs.getInt(1);
             }
         } catch (Exception ex) {
-            System.out.println("Exception (PP id): " + ex);
+            //System.out.println("Exception (PP id): " + ex);
         }
-        System.out.println("Lortutako PP id-a: " + id);
+        //System.out.println("Lortutako PP id-a: " + id);
         return id;
     }
 
@@ -112,9 +115,9 @@ public class ProduktuListaInportatu {
                 id = rs.getInt(1);
             }
         } catch (Exception ex) {
-            System.out.println("Exception (SQ id): " + ex);
+            //System.out.println("Exception (SQ id): " + ex);
         }
-        System.out.println("Lortutako SQ id-a: " + id);
+        //System.out.println("Lortutako SQ id-a: " + id);
         return id;
     }
 
